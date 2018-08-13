3187df8d6a1c6391ee34653f93c3df88d36d8c3f
Kristin Strickland <kstrickland@liveperson.com>
2018-08-13 12:52:14 -0400

    Added code for in-person challenge-two

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,18 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var WebSocket = require('ws');
+var fs = require('file-system');
+var OAuth  =  require('oauth-1.0a');
+var crypto = require('crypto');
 var ws;
 
-/* GET home page. */
+/* GET conversation and post first message. */
 router.get('/', function(req, res, next) {
     retrieveJwtToken(connectToMessagingService);
+});
+
+router.get('/conversation', function(req, res, next)  {
+   getConversation(res);
 });
 
 /**
@@ -72,9 +79,7 @@ function connectToMessagingService(token) {
  * ConsumerRequestConversation type message request
  * to the WebSocket.
  *
- * @param message
- *
- * returns string - ConversationId
+ * returns void
  */
 function createNewConversation() {
     console.log("Creating new conversation...");
@@ -118,6 +123,60 @@ function publishTextMessage(conversationId) {
         if (error) console.log("WebSocket sending error: ", error);
     });
     ws.close();
+}
+
+/**
+ * getConversation
+ *  This method retrieves a list of conversations with all their metadata and related messages.
+ *
+ * return object - Conversation metadata
+ */
+function getConversation(res) {
+    var url = 'https://va.msghist.liveperson.net/messaging_history/api/account/13350576/conversations/conversation/search?v=2';
+    var body = { conversationId: '3c026cc6-f1b8-4ddd-a24b-30068420d09c' };
+    var oauth = OAuth({
+        consumer: {
+            key: '90dc82090ec74284a2ae8bd22e770642',
+            secret: '5a62d04af7b9c39e'
+        },
+        signature_method: 'HMAC-SHA1',
+        hash_function(base_string, key) {
+            return crypto.createHmac('sha1', key).update(base_string).digest('base64');
+        }
+    });
+
+    var token  = {
+        key: '39e1f5ef76194d1f80da029eff3c0e3c',
+        secret: '40194cd29bf78514'
+    };
+
+    var options = {
+        url: url,
+        method: 'POST',
+        data: body
+    };
+
+    request({
+        url: options.url,
+        method: options.method,
+        form: oauth.authorize(options, token)
+    }, function(error, response, body) {
+        if (error) { console.log(error); res.send(error)}
+        createFileFromJSON(body);
+        res.send(response);
+    });
+}
+
+/**
+ * createFileFromJSON
+ * Creates a .txt file using JSON Object
+ *
+ * @param obj
+ *
+ * return void
+ */
+function createFileFromJSON(obj) {
+    fs.writeFile('output.txt', obj);
 }
 
 module.exports = router;
